1014a31046dba69c92e507d46e6f586f3226b807
Bonuspunkt <bonuspunkt+github@gmail.com>
2016-03-02 05:11:31 +0100

    adding nodemon

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: './wwwScript/index.js',
     output: {
@@ -13,9 +15,14 @@ module.exports = {
             }
         }, {
             test: /\.styl$/,
-            loader: 'style-loader!css-loader!stylus-loader'
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
         }]
     },
+    plugins: [
+        new ExtractTextPlugin('wwwRoot/stylesheet.css', {
+            allChunks: true
+        })
+    ],
     resolve: {
         extensions: ['', '.js', '.jsx']
     }
