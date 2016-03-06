a37c82ed2281a31439fea15d22ab6a69a06148e4
Bonuspunkt <bonuspunkt+github@gmail.com>
2016-03-06 23:32:15 +0100

    link games to steam

--- wwwScript/component/gameServer.jsx
+++ wwwScript/component/gameServer.jsx
@@ -2,9 +2,16 @@
 const React = require('react');
 
 const GameServer = (props) => {
+
+    let game = props.game;
+    if (props.steamAppId) {
+        const url = `http://store.steampowered.com/app/${props.steamAppId}/`;
+        game = (<a href={ url }>{props.game}</a>);
+    }
+
     return (
         <tr key={ `${props.address}:${props.port}` }>
-            <td>{props.game}</td>
+            <td>{game}</td>
             <td>{props.name}</td>
             <td>{props.map}</td>
             <td>{props.players}/{props.maxPlayers}</td>
